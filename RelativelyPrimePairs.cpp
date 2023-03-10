10e532150339ac95cae4154fb678c06e6ffeaeb7
Iti singh <106474251+Itisingh3@users.noreply.github.com>
2023-03-10 10:25:37 +0530

    Update RelativelyPrimePairs.cpp

--- RelativelyPrimePairs.cpp
+++ RelativelyPrimePairs.cpp
@@ -1,8 +1,15 @@
-void solve(long l,long r ){
-  cout<<"YES"<<endl;
-  vector<int,int> ans;
-  for(int i=0;i<r-l+1;i+=2){
-    ans.push_back({l+i,l+i+1});
-  }
-  return ans;
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+       int l,r;
+       cin>>l>>r;
+       cout<<"YES"<<endl;
+       for(int i=0;i<r-l+1;i+=2){
+            cout<<l+i<<" "<<l+i+1<<endl;
+       }
 }
