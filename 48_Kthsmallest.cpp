837b935d86ee475a3e8582ceb8a014ec39b21f15
Iti singh <106474251+Itisingh3@users.noreply.github.com>
2023-04-03 18:29:41 +0530

    Update 48_Kthsmallest.cpp

--- 48_Kthsmallest.cpp
+++ 48_Kthsmallest.cpp
@@ -1,4 +1,40 @@
- int kthSmallest(int arr[], int l, int r, int k) {
-        sort(arr,arr+r+1);
-        return arr[k-1];
-    }
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int Kth_smallest(map<int, int> mp, int K)
+{
+	int freq = 0;
+	for (auto it = mp.begin(); it != mp.end(); it++) {
+		freq += (it->second); // adding the frequencies of
+							// each element
+
+		if (freq >= K) 
+		{
+			return it->first;
+		}
+	}
+
+	return -1; 
+}
+
+int main()
+{
+	int N = 5;
+	int K = 2;
+	vector<int> arr = { 12, 3, 5, 7, 19 };
+
+	map<int, int> mp;
+	for (int i = 0; i < N; i++) {
+		mp[arr[i]] += 1; 
+	}
+
+	// Function call
+	int ans = Kth_smallest(mp, K);
+	cout << "The " << K << "th smallest element is " << ans
+		<< endl;
+
+	return 0;
+}
+
