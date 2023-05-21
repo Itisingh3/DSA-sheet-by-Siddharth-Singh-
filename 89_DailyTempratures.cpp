38c813e98e01081e4854f778863b94c9bd34a319
Iti singh <106474251+Itisingh3@users.noreply.github.com>
2023-05-21 22:34:34 +0530

    Create 89_DailyTempratures.cpp

---
+++ 89_DailyTempratures.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& nums) {
+        stack<int>st;
+        vector<int> ans;
+        for(int i=nums.size()-1;i>=0;i--){
+            while(!st.empty() && nums[st.top()]<=nums[i]){
+                st.pop();
+            }
+            if(st.empty()){
+                ans.push_back(0);
+            }
+            else{
+                ans.push_back(st.top()-i);
+            }
+            st.push(i);
+        }
+        reverse(ans.begin(),ans.end());
+return ans;
+    }
+};
