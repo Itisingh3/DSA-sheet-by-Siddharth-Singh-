9316c666e8611097344f3f811aed3c9a07a547aa
Iti singh <106474251+Itisingh3@users.noreply.github.com>
2023-07-08 23:13:32 +0530

    Update 112_Rightview.cpp

--- 112_Rightview.cpp
+++ 112_Rightview.cpp
@@ -11,8 +11,8 @@
  */void func(Node* root, vector<int>&ans,int level){
     if(!root)return ;
     if(ans.size()==level)ans.push_back(root->data);
-    func(root->left,ans,level+1);
     func(root->right,ans,level+1);
+    func(root->left,ans,level+1);
 }
 vector<int> leftView(Node *root)
 {
